cfe51043197b0a7b04d7d4e79c557ca48f5b2552
Okuhle <okuhlen.20@gmail.com>
2023-02-08 09:32:50 +0000

    pushing tests

---
+++ models/base_model.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+Class Module Doc: 
+"""
+from uuid import uuid4
+from datetime import datetime
+
+
+
+class BaseModel:
+
+	name = None
+	my_number = None
+
+	def __init__(self):
+		
+		self.id = str(uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
+	
+	def __str__(self):
+
+		name = __class__.__name__
+		attrs = self.__dict__
+
+		return f"[{name}] ({self.id}) {attrs}"
+
+	def save(self):
+
+		self.updated_at = datetime.now()
+
+	def to_dict(self):
+
+		res = self.__dict__
+		res["created_at"] = str(res["created_at"])
+		res["updated_at"] = str(res["updated_at"])
+		res["__class__"] = __class__.__name__
+
+		return res
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+print()
+my_model.save()
+print(my_model)
+print()
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print()
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
