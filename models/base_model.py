d14849fd3751cf0d57ef3e7680e01a4aeaad4b1e
Okuhle <okuhlen.20@gmail.com>
2023-02-12 20:48:55 +0000

    fixed circular import error

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,6 @@
 """
 from uuid import uuid4
 from datetime import datetime
-try:
-    import models
-except (ImportError, ModuleNotFoundError):
-    import sys
-    sys.path.append("..")
 from models import storage
 
 
@@ -49,6 +44,7 @@ class BaseModel:
 
         name = __class__.__name__
         attrs = self.__dict__
+
         return f"[{name}] ({self.id}) {attrs}"
 
     def save(self):
@@ -66,28 +62,3 @@ class BaseModel:
         res["created_at"] = self.created_at.isoformat()
 
         return res
-
-    def str_time(self, val):
-
-        val = val.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return val
-
-    def set_time(self, val):
-
-        if type(val) != datetime:
-            val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
-        return val
-
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    #print("creatiion:", type(obj.created_at))
-    print(obj)
-
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
